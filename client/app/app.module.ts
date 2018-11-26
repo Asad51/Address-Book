0258424ab2bbeab90f8c6a29f7ee64a9247ec89b
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-11-27 01:45:13 +0600

    routing

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms'
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,22 @@ import { FooterComponent } from './common/footer/footer.component';
 import { from } from 'rxjs';
 import { LoginComponent } from './modules/login/login.component';
 import { RegistrationComponent } from './modules/registration/registration.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { ContactsComponent } from './modules/contacts/contacts.component';
+import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.component';
+import { FeaturesComponent } from './modules/features/features.component';
+import { TermsComponent } from './modules/terms/terms.component';
+
+const routes: Routes = [
+  { path: 'register', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'features', component: FeaturesComponent },
+  { path: 'terms', component: TermsComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -18,12 +35,21 @@ import { RegistrationComponent } from './modules/registration/registration.compo
     HeaderComponent,
     FooterComponent,
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    DashboardComponent,
+    ContactsComponent,
+    PageNotFoundComponent,
+    FeaturesComponent,
+    TermsComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
