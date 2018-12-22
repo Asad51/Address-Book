b901e101374a3a1a611b5178203fc1b47133530d
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-12-22 23:02:33 +0600

    Routing error fixed

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -9,27 +9,35 @@ import { ContactsComponent } from "./modules/contacts/contacts.component";
 import { PageNotFoundComponent } from "./modules/page-not-found/page-not-found.component";
 import { AddContactComponent } from "./modules/contacts/add-contact/add-contact.component";
 import { EditContactComponent } from "./modules/contacts/edit-contact/edit-contact.component";
-import { StarterComponent } from './modules/contacts/starter/starter.component';
+import { StarterComponent } from "./modules/contacts/starter/starter.component";
 import { EditProfileComponent } from "./modules/dashboard/edit-profile/edit-profile.component";
 import { ChangePasswordComponent } from "./modules/dashboard/change-password/change-password.component";
 
 import { AuthGuardService, NoAuthService } from "./core/authentication";
 
 const routes: Routes = [
-  { path: "register", component: RegistrationComponent, canActivate: [NoAuthService] },
+  {
+    path: "register",
+    component: RegistrationComponent,
+    canActivate: [NoAuthService]
+  },
   { path: "login", component: LoginComponent, canActivate: [NoAuthService] },
   {
-    path: "dashboard", component: DashboardComponent, 
-    canActivate: [AuthGuardService], canActivateChild: [AuthGuardService],
+    path: "dashboard",
+    canActivate: [AuthGuardService],
+    canActivateChild: [AuthGuardService],
     children: [
       { path: "edit", component: EditProfileComponent },
-      { path: "change-password", component: ChangePasswordComponent }
+      { path: "change-password", component: ChangePasswordComponent },
+      { path: "", component: DashboardComponent }
     ]
   },
   {
-    path: "contacts", component: ContactsComponent, canActivate: [AuthGuardService],
+    path: "contacts",
+    component: ContactsComponent,
+    canActivate: [AuthGuardService],
     children: [
-      { path: '', component: StarterComponent },
+      { path: "", component: StarterComponent },
       { path: "add", component: AddContactComponent },
       { path: ":id", component: EditContactComponent }
     ]
